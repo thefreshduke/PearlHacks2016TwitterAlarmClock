68522347521585f2d8f7b3f3fa01992244db4c6e
thefreshduke <scottyshaw1@gmail.com>
2016-04-02 05:24:55 -0400

    Completed basic versions of TwitterViewController and AlarmViewController; created conditional segues with Storyboard Segue Identifiers; added layout navigation

--- PearlHacks2016TwitterAlarmClock/AlarmViewController.swift
+++ PearlHacks2016TwitterAlarmClock/AlarmViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  AlarmViewController.swift
 //  PearlHacks2016TwitterAlarmClock
 //
 //  Created by Scotty Shaw on 4/2/16.
@@ -8,29 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
-    var isLoggedIn: Bool = false
-    
-    @IBOutlet weak var startButton: UIButton!
-
-    @IBAction func goToNextView(sender: AnyObject) {
-        if (isLoggedIn) {
-            self.performSegueWithIdentifier("LoggedInSegue", sender: self)
-        }
-        else {
-            self.performSegueWithIdentifier("NotLoggedInSegue", sender: self)
-        }
-    }
+class AlarmViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 }
-
