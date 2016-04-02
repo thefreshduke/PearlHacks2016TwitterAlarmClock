729c20476328735736e99756aa89557571a83e9e
thefreshduke <scottyshaw1@gmail.com>
2016-04-02 07:27:14 -0400

    Implemented non-stop waves audio, which may be turned off upon segue with .stop(); need to truncate the silent tail of the audio file

--- PearlHacks2016TwitterAlarmClock/ViewController.swift
+++ PearlHacks2016TwitterAlarmClock/ViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var wavesAudioPlayer: AVAudioPlayer?
     
     var startTime = NSTimeInterval()
     
@@ -17,7 +20,7 @@ class ViewController: UIViewController {
     var isLoggedIn: Bool = false
     
     @IBOutlet weak var startButton: UIButton!
-
+    
     @IBAction func goToNextView(sender: AnyObject) {
         if (isLoggedIn) {
             self.performSegueWithIdentifier("LoggedInSegue", sender: self)
@@ -32,8 +35,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         validateTimer()
+        
+        if let waves = self.setupAudioPlayerWithFile("waves", type:"mp3") {
+            self.wavesAudioPlayer = waves
+        }
+        
+        wavesAudioPlayer?.numberOfLoops = -1
+        wavesAudioPlayer?.volume = 1
+        wavesAudioPlayer?.play()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -52,17 +63,38 @@ class ViewController: UIViewController {
         let elapsedTime = currentTime - startTime
         let sizeOffset = elapsedTime % 10.0
         var sizeAdjustment: Double = 0.0
-//        var alphaAdjustment: Double = 0.0
+        //        var alphaAdjustment: Double = 0.0
         if (sizeOffset < 5) {
             sizeAdjustment = (80.0 + 4 * sizeOffset) / 100.0
-//            alphaAdjustment = (80.0 + 4 * sizeOffset) / 100.0
+            //            alphaAdjustment = (80.0 + 4 * sizeOffset) / 100.0
         }
         else {
             sizeAdjustment = (120.0 - 4 * sizeOffset) / 100.0
-//            alphaAdjustment = (120.0 - 4 * sizeOffset) / 100.0
+            //            alphaAdjustment = (120.0 - 4 * sizeOffset) / 100.0
         }
         startButton.titleLabel!.font =  UIFont(name: (startButton.titleLabel?.font?.fontName)!, size: 75.0 * CGFloat(sizeAdjustment))
-//        startButton.titleLabel!.textColor = UIColor(red: 0.0, green: 1, blue: 1, alpha: CGFloat(alphaAdjustment))
+        //        startButton.titleLabel!.textColor = UIColor(red: 0.0, green: 1, blue: 1, alpha: CGFloat(alphaAdjustment))
+    }
+    
+    func setupAudioPlayerWithFile(file: NSString, type: NSString) -> AVAudioPlayer?  {
+        
+        // 1
+        let path = NSBundle.mainBundle().pathForResource(file as String, ofType: type as String)
+        let url = NSURL.fileURLWithPath(path!)
+        
+        // 2
+        var audioPlayer: AVAudioPlayer?
+        
+        // 3
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOfURL: url)
+        }
+        catch {
+            print("Player not available")
+        }
+        
+        // 4
+        return audioPlayer
     }
 }
 
